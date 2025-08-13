de51409d9123b275c7556a688eee0041b4beacb2
henrygsr <henry@gsrconstruct.com>
2025-08-13 14:09:13 -0500

    Update forms.py

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,15 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email
-
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
-    submit = SubmitField("Sign In")
+    remember = BooleanField("Remember me")
+    submit = SubmitField("Log In")
 
-
+# If you don't use registration, you can delete this class
+# and also remove it from any imports/routes.
 class RegistrationForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
-    password = PasswordField("Password", validators=[DataRequired()])
+    username = StringField("Username", validators=[DataRequired(), Length(min=2, max=50)])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField("Confirm Password",
+                                     validators=[DataRequired(), EqualTo("password")])
     submit = SubmitField("Create Account")
